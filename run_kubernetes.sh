9fe5c8c660dea2dd56454f6289118bd12bab4245
MuhammadZubair210 <mzubairabdullah210@gmail.com>
2021-12-13 04:18:12 +0500

    first commit

---
+++ run_kubernetes.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=zubairra786/udacity:task
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run udacity\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=9090 --labels app=udacity
+
+# Step 3:
+# List kubernetes pods
+
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward udacity 8000:9090
+
